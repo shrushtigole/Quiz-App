c099a7ef5c861dc8c7e6f31152b1572c94019c99
shrushtigole <shrushtigole@gmail.com>
2024-12-29 02:19:11 +0530

    Verification and Validation of user is done, and started designin the quiz questions

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function signup() {
     function validateSignup(fullName, email, password) {
         const regexname = /^[a-zA-Z]{2,50}(?: [a-zA-Z]{2,50})*(?:[-' ][a-zA-Z]{2,50})*$/;   //name: small and caps letters with size 
         const regexmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;   //email: small and caps letters berfore and after @ and .
-        const regexpassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;  //password: one lowercase, one uppercase, atlest one digit, one special character.
+        const regexpassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/;  //password: one lowercase, one uppercase, atlest one digit, one special character.
     
         
         const isnameValid = regexname.test(fullName);
@@ -52,7 +52,11 @@ function signup() {
     let userInfo = JSON.parse(localStorage.getItem("userInfo")) || [];
 
     //Check if the user already exist based on Email   
-    const userExists = userInfo.some(user => user.mail === email)
+    const userExists = userInfo.some(user => user.mail === email);
+    if (userExists){
+        alert("This Email Already Exists");
+        return;
+    }
 
     var userDetail = {
         name: fullName,
@@ -63,8 +67,152 @@ function signup() {
     userInfo.push (userDetail);
 
     localStorage.setItem("userInfo",JSON.stringify(userInfo));
-    alert("Signup Successful!")
+    alert("Signup Successful!");
+
+    window.location="/Pages/login.html";
 }
+
+
+
+//#########################Login page###########################//
+function login(){
+    console.log("function")
+    
+    var email = document.getElementById("Email").value;
+    var password = document.getElementById("Password").value;
+    
+    let userInfo = JSON.parse(localStorage.getItem("userInfo")) || [];
+    const user = userInfo.find(user => user.mail === email);
+
+    if(!user){
+        alert("Invalid Email !");
+        return;
+    }
+
+    if(user.password === password){
+        alert("Login Successfull !");
+        window.location="/Pages/Starting-quiz.html";
+    }
+    else{
+        alert("Incorrect Password !");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const questions =[
+    {
+        question: "Which of the following is an output device?",
+        answer:[
+            {option:"Keyboard", correct:false},
+            {option:"Joystick", correct:false},
+            {option:"Printer", correct:true},
+            {option:"None of the above", correct:false}
+            ]
+    },
+    {
+        question: "Which software must first be installed to a new computer from a manufacturer",
+        answer:[
+            {option:"Device Driver", correct:false},
+            {option:"Operating System", correct:true},
+            {option:"Application Software", correct:false},
+            {option:"Utilities", correct:false}
+        ]
+    },
+    {
+        question: "A program that can copy itself and infect a computer without permission or knowledge of the owner is called what?",
+        answer:[
+            {option:"Floppy", correct:false},
+            {option:"Virus", correct:true},
+            {option:"Java", correct:false},
+            {option:"Monitor", correct:false}
+        ]
+    },
+    {
+        question: "Instructions written by a programer for a computer use is called?",
+        answer:[
+            {option:"Hardware", correct:false},
+            {option:"Software Instruction", correct:false},
+            {option:"Software", correct:true},
+            {option:"System Software", correct:false}
+        ]
+    },
+    {
+        question: "A Supervisor is a central component of",
+        answer:[
+            {option:"Software", correct:false},
+            {option:"Operating System", correct:true},
+            {option:"Device Driver", correct:false},
+            {option:"Utilities", correct:false}
+        ]
+    },
+    {
+        question: "What does HTTP stands for?",
+        answer:[
+            {option:"Hypertext Transfer Protocol", correct:true},
+            {option:"Hypertext Transfer Plotter", correct:false},
+            {option:"Head Tail Tranfer Plot", correct:false},
+            {option:"Head Tail Transfer Protocol", correct:false}
+        ]
+    },
+    {
+        question: "The software which provides specific services to its end user is called?",
+        answer:[
+            {option:"Application Software", correct:true},
+            {option:"System Software", correct:false},
+            {option:"Device driver", correct:false},
+            {option:"Microsoft Office", correct:false}
+        ]
+    },
+    {
+        question: "What is normally used to configure a windows installation",
+        answer:[
+            {option:"System Configuration", correct:true},
+            {option:"Safe mode", correct:false},
+            {option:"Start up", correct:false},
+            {option:"Live Mode", correct:false}
+        ]
+    },
+    {
+        question: "Which was the first Web browser?",
+        answer:[
+            {option:"WorldWideWeb", correct:true},
+            {option:"Netscape Navigator", correct:false},
+            {option:"Internet Explorer", correct:false},
+            {option:"Safari", correct:false}
+        ]
+    },
+    {
+        question: "Which Operating Systems Source code is visible to its users?",
+        answer:[
+            {option:"Linux", correct:false},
+            {option:"Ubuntu", correct:true},
+            {option:"Windows", correct:false},
+            {option:"Mac OS", correct:false}
+        ]
+    },
+]
 
 
 
